73635401cccdc97b889614c4e284ae43e6d19f52
Alisson-Henrique <torgatesousau@gamil.com>
2020-09-28 21:25:56 -0300

    Main menu UI elements

--- Assets/Scripts/Conn.cs
+++ Assets/Scripts/Conn.cs
@@ -18,6 +18,25 @@ public class Conn : MonoBehaviourPunCallbacks
     {
         
     }
- 
+
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("Estou no Lobby!");
+
+
+
+    }
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Estou Conectado!");
+    }
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        Debug.Log("Disconectei!");
+    }
+    public override void OnJoinRoomFailed(short returnCode, string message)
+    {
+        base.OnJoinRoomFailed(returnCode, message);
+    }
 
 }
